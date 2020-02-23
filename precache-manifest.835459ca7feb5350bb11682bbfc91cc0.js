c938993fa349fe549bb9680dcba4bc7510ca697a
Mauricio <mofalius@gmail.com>
2020-02-23 00:56:14 -0300

    Primer commit

---
+++ precache-manifest.835459ca7feb5350bb11682bbfc91cc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfe697963627906348f119ee3226e7b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3af1bd3d64641ce578c5",
+    "url": "/static/css/main.fad4cd55.chunk.css"
+  },
+  {
+    "revision": "8c174664cc2b6506d846",
+    "url": "/static/js/2.821ae078.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.821ae078.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3af1bd3d64641ce578c5",
+    "url": "/static/js/main.f18bd3fd.chunk.js"
+  },
+  {
+    "revision": "899047bc370c5d65ae90",
+    "url": "/static/js/runtime-main.98d2fd33.js"
+  }
+]);
